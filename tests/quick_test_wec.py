90cfe920f434889c34b5ea27618b4bb5aa615a08
Lucas Puentes <lucaspuentes@yahoo.com>
2017-10-30 16:46:13 -0400

    More functionality in visual
    Some small improvements made to visual. Many more to come.

--- tests/quick_test_wec.py
+++ tests/quick_test_wec.py
@@ -1,15 +1,17 @@
 # This test is for quickly testing functionality of individual rules
 import wec.wec
+import wec.wec_visual
+import time
 
 
 def test_simulation_of_wec():
     device = wec.wec.WEC()
-    device.add_body('cylinder', 10, (0, 0.0), radius=1, length=2)
+    device.add_body('sphere', 10, (400, 400.0), radius=25)
+    device.add_linear_body('sphere', 20, (500, 400.0), 0, 1, 50, 10000, 10000, radius=25)
+    device.add_rotational_body('sphere', 10, (300, 400.0), 2, 0, 0, 20000, 40000, radius=25)
+    device.add_linear_body('sphere', 20, (400, 300.0), 3, 0, 50, 15000, 20000, radius=25)
 
-    print(device.bodies[0])
+    display = wec.wec_visual.wec_visual()
+    display.display(device)
 
-    device.change_body_dimensions(0, radius=2, length=4)
-
-    print(device.bodies[0])
-
-    device.evaluate()
+    time.sleep(5)
--- wec/wec_visual.py
+++ wec/wec_visual.py
@@ -1,17 +1,21 @@
 import pygame
+import pymunk.pygame_util
+
 
 class wec_visual():
 
     def __init__(self):
         # initialize screen for displaying WEC design
         pygame.init()
-        screen = pygame.display.set_mode((1000, 800))
-        screen.fill((255, 255, 255))
+        self.screen = pygame.display.set_mode((1000, 800))
+        self.screen.fill((255, 255, 255))
         pygame.display.update()
 
-    def display(self):
-        pass
+    def display(self, device):
 
-screen = wec_visual()
-while True:
-        pass
+        options = pymunk.pygame_util.DrawOptions(self.screen)
+        device.world.step(1)
+        device.world.step(2)
+        device.world.debug_draw(options)
+        pygame.display.update()
+
